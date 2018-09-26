bc021c4419c84f1f6d8eb14b2b7a14995c74cd89
wan.183 <wan.183@osu.edu>
2018-09-26 16:30:25 -0400

    added watson functionality to respond

--- watsonwebapp/public/javascripts/conversationFunctions.js
+++ watsonwebapp/public/javascripts/conversationFunctions.js
@@ -1,10 +1,12 @@
 avatar_img_src = '/images/person1_avatar.png';
+watson_img_src = '/images/watson_avatar.png';
 
 function button_send() {
   var user_message = user_retrieve_message();
   if (user_message.length > 0) {
     user_display_message(user_message);
   }
+  watson_message('Watson\'s message goes here');
   return user_message;
 }
 
@@ -39,8 +41,29 @@ function user_display_message(userInputText) {
   body.appendChild(newDiv);
 }
 
-function watson_message() {
+function watson_message(watsonText) {
+  // create div for Watson's message
+  var responseDiv = document.createElement('div');
+  responseDiv.className = "container";
+
+  // create Watson's avatar img
+  var responseImg = document.createElement('img');
+  responseImg.setAttribute("src", watson_img_src);
+  responseImg.setAttribute("alt", "Avatar");
+  responseImg.setAttribute("class", "left");
+  responseImg.setAttribute("style", "width:100%");
+  responseDiv.appendChild(responseImg);
   
+  // create Watson's paragraph
+  var responseP = document.createElement('p')
+  responseP.setAttribute("margin", "10px 0");
+  responseP.appendChild(document.createTextNode(watsonText));
+  responseDiv.appendChild(responseP);
+
+  // put up Watson's new message
+  var body = document.getElementsByClassName('chatMsgs')[0];
+  body.appendChild(responseDiv);
+
 }
 
 
--- watsonwebapp/routes/conversation.js
+++ watsonwebapp/routes/conversation.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('conversation');
+  res.render('conversation', { title: 'Conversation with Watson' });
 });
 
 module.exports = router;

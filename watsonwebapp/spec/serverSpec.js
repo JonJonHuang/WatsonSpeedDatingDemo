b62e0ed492db035f9c0d530dbe4f547b62e63476
Jon <jon55999@yahoo.com>
2018-09-27 16:20:59 -0400

    Jenkins demo

--- watsonwebapp/spec/serverSpec.js
+++ watsonwebapp/spec/serverSpec.js
@@ -4,6 +4,7 @@ var childProcess = require('child_process');
 describe('Stand server', function() {
     // TODO - dont assume in watsonwebapp dir, use Path library to find www
     var subprocess = childProcess.fork('bin/www');
+    before
     // TODO - race condition, test fails if it runs before subprocess
     it('Server stood up', function(done) {
         http.get('http://127.0.0.1:3000', function(res) {

70ec4700610bc9f1174c2f84d5211584dfcdcceb
wan.183 <wan.183@osu.edu>
2018-10-23 16:25:09 -0400

    Added React js frontend framework for client

---
+++ watsonwebapp/client/javascripts/conversationFunctions.js
@@ -0,0 +1,100 @@
+/*
+Constants
+TO-DO: refactor these variables to another configuration file?
+*/
+avatar_img_src = '/images/person1_avatar.png';
+watson_img_src = '/images/watson_avatar.png';
+
+var username = prompt('Enter your username');
+
+/*
+This function runs when the user clicks the "Send" button.
+It will grab the message from the input text box (user_retrieve_message())
+then display that message into the chat logs (user_display_message())
+*/
+async function button_send() {
+  var user_message = user_retrieve_message();
+  if (user_message.length > 0) {
+    user_display_message(user_message);
+    auto_scroll_chatMsgs();
+    $.post('', {text: user_message, user: username},
+      function(data, status, xhr) {
+        watson_message(data[0]);
+        auto_scroll_chatMsgs();
+      }
+    );
+  }
+  return user_message;
+}
+
+function auto_scroll_chatMsgs() {
+  var chatMsgs = document.getElementsByClassName("chatMsgs")[0];
+  chatMsgs.scrollTop = chatMsgs.scrollHeight;  
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/*
+Returns the message and clears it from input text box
+*/
+function user_retrieve_message() {
+  var userInputText = document.getElementById("user_input").value;
+  document.getElementById("user_input").value = "";
+  return userInputText;
+}
+
+/*
+Displays the input message into the chat logs
+*/
+function user_display_message(userInputText) {
+  // create new div for chat message
+  var newDiv = document.createElement('div');
+  newDiv.className = "container darker";
+
+  // create the avatar img
+  var newImg = document.createElement('img');
+  newImg.setAttribute("src", avatar_img_src);
+  newImg.setAttribute("alt", "Avatar");
+  newImg.setAttribute("class", "right");
+  newImg.setAttribute("style", "width:100%");
+  newDiv.appendChild(newImg);
+  
+  // create the paragraph
+  var newP = document.createElement('p')
+  newP.setAttribute("margin", "10px 0");
+  newP.appendChild(document.createTextNode(userInputText));
+  newDiv.appendChild(newP);
+
+  // put up the user's new message
+  var body = document.getElementsByClassName('chatMsgs')[0];
+  body.appendChild(newDiv);
+}
+
+/*
+This function currently just displays some text to be delivered by Watson
+*/
+function watson_message(watsonText) {
+  // create div for Watson's message
+  var responseDiv = document.createElement('div');
+  responseDiv.className = "container";
+
+  // create Watson's avatar img
+  var responseImg = document.createElement('img');
+  responseImg.setAttribute("src", watson_img_src);
+  responseImg.setAttribute("alt", "Avatar");
+  responseImg.setAttribute("class", "left");
+  responseImg.setAttribute("style", "width:100%");
+  responseDiv.appendChild(responseImg);
+  
+  // create Watson's paragraph
+  var responseP = document.createElement('p')
+  responseP.setAttribute("margin", "10px 0");
+  responseP.appendChild(document.createTextNode(watsonText));
+  responseDiv.appendChild(responseP);
+
+  // put up Watson's new message
+  var body = document.getElementsByClassName('chatMsgs')[0];
+  body.appendChild(responseDiv);
+}
---
+++ watsonwebapp/client/src/App.js
@@ -0,0 +1,213 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import logo from './logo.svg';
+import './App.css';
+
+const DUMMY_DATA = [
+  {
+    senderId: "Watson",
+    text: "Hello, I'm Watson. How can I help you today?"
+  }
+]
+
+class MainApp extends Component {
+  constructor() {
+    super()
+    this.state={
+      page: 1
+    }
+  }
+  
+  render() {
+    var dummy = " "
+    switch(this.state.page) {
+      case 1:
+        dummy = <HomeApp />
+        break;
+      case 2:
+        dummy = <ConversationApp />
+        break
+      case 3:
+        dummy = <PersonalityApp />
+        break;
+      default:
+        dummy = <HomeApp />
+    }
+    return (
+      <>
+        <div className="sidenav">
+          <button onClick={() => this.setState({page: 1})} >Home</button>
+          <br/>
+          <button onClick={() => this.setState({page: 2})} >Conversation with Watson</button>
+          <br/>
+          <button onClick={() => this.setState({page: 3})} >Personality and Matches</button>
+        </div>
+        {dummy}
+      </>
+    )
+  }
+}
+
+function SideNav() {
+  return (
+    <p>dummy</p>
+  );
+    // a(href='/conversation/get-groups') Check for Matches
+    // a(href='/personality') User Personality
+}
+
+class ConversationApp extends Component {
+  constructor() {
+    super()
+    this.state = {
+      messages: DUMMY_DATA,
+      current_input: ''
+    }
+    this.handleChangeXXX = this.handleChangeXXX.bind(this)
+    this.handleKeyPressXXX = this.handleKeyPressXXX.bind(this)
+  }
+
+  render() {
+    // console.log(this.state.current_input)
+    return (
+      <div className="ConversationApp">
+        <Title />
+        <MessageList messages = {this.state.messages} />
+        <SendMessageForm
+          value={this.state.current_input}
+          onChange={() => this.handleChangeXXX}
+          onKeyPress={() => this.handleKeyPressXXX}
+        />
+        <button
+          type="button"
+          onClick={ function() {
+            console.log("ans");
+            ReactDOM.render(<Title />, document.getElementById('root'));
+          } }
+        >
+          Debugger
+        </button>
+      </div>
+    );
+  }
+
+  handleChangeXXX(e) {
+    // on change, update the state's message
+    this.setState({
+      current_input: e.target.value
+    })
+    console.log("handleChangeXXX: " + this.state.current_input)
+  }
+  
+  handleKeyPressXXX(e) {
+    // 13 is the charCode for the Enter key
+    if (e.charCode === 13 && this.state.current_input.length > 0) {
+      console.log("ENTER KEY")
+      // send the state's message for submission
+      console.log("handleKeyPressXXX: " + this.state.current_input)
+      
+      // SEND HTTP POST
+      
+      // UPDATE MESSAGELIST
+      
+      // update messages; clear current_input
+      const messages_copy = this.state.messages.slice();
+      messages_copy.push({senderId: "User", text: this.state.current_input})
+      this.setState({
+        messages: messages_copy,
+        current_input: ''
+      })
+    }
+  }
+
+}
+
+class MessageList extends React.Component {
+  render() {
+    return (
+      <ul className="message-list">
+        {this.props.messages.map(message => {
+          return (
+            <MessageItem
+              senderId={message.senderId}
+              message={message.text}
+            />
+          )
+        })}
+      </ul>
+    )
+  }
+  
+  /* <li key={message.id}>
+              <div>
+                {message.senderId}
+              </div>
+              <div>
+                {message.text}
+              </div>
+            </li> */
+  
+}
+
+class MessageItem extends Component {
+  render() {
+    return (
+      <div className="message-item">
+        <div className="message-sender">{this.props.senderId}</div>
+        <div className="message-text">{this.props.message}</div>
+      </div>
+    );
+  }
+}
+
+function SendMessageForm(props) {
+  // console.log(props)
+  return(
+    <input
+      className="send-message-form"
+      type="text"
+      value={props.value}
+      onChange={props.onChange()}
+      onKeyPress={props.onKeyPress()}
+    />
+  );
+  
+    // onChange={this.handleChange}
+    // onKeyPress={this.handleKeyPress}
+    
+    /* <form
+        className="send-message-form">
+        <input
+          onChange={this.handleChange}
+          value={this.state.message}
+          placeholder="Type your message and hit ENTER"
+          type="text" />
+      </form> */
+  
+}
+
+function Title() {
+  return(
+    <p className="title">Conversation with Watson</p>
+  )
+}
+
+class PersonalityApp extends React.Component {
+  render() {
+    return (<p>PersonalityApp</p>)
+  }
+}
+
+class HomeApp extends React.Component {
+  render() {
+    return (
+      <>
+        <p>Welcome to Watson Speed Friending, created by Jonathon Huang, Aakash Singh, Stephen Wan, and Ziheng Wei</p>
+        <p>The "Conversation with Watson" tab will start a conversation with Watson to begin collecting interests and personality information about the user.</p>
+        <p>The "User Personality" tab will display information that Watson has gathered from the conversation about a user's interests and personality.</p>
+      </>
+    )
+  }
+}
+
+export default MainApp;

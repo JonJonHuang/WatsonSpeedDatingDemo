3a35ddc37fe1d0f7ca7af1d57e63058b17955dc8
andywei1103 <andywei_1103@sina.com>
2018-10-24 19:39:18 -0400

    adding jasmine tests

---
+++ watsonwebapp/spec/matchingSpec.js
@@ -0,0 +1,33 @@
+var personality = require('./personality');
+var dummyDataA = {
+
+}
+var dummyDataB = {
+
+}
+var dummyDataC = {
+
+}
+var dummyDataD = {
+
+}
+var dummyDataE = {
+
+}
+var dummyDataF = {
+
+}
+
+describe('personality',function(){
+    beforeAll(function(done){
+
+    })
+    it('matched successfully', function(done){
+
+
+        done();
+    })
+    afterAll(function(done){
+        done();
+    })
+})

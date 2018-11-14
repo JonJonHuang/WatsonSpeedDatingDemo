e239c6a57bbe79f639b3e64f32aca1bf2f850bde
Jon <jon55999@yahoo.com>
2018-11-14 16:41:20 -0500

    Server now checks email against a regex at registration.

--- watsonwebapp/server/routes/register.js
+++ watsonwebapp/server/routes/register.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 const dbUtils = require('../../util/db');
 
-const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/
+const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
 router.post('/', async (req, res, next) => {
+  if (!emailRegex.test(req.body.email)) {
+    // User failed to provide a legal email address
+    res.send({success: false, error: 'User provided an invalid email address.'});
+    return;
+  }
   let success = await dbUtils.registerUser(req.body.email, req.body.username, req.body.password);
   res.send({success: success});
 });

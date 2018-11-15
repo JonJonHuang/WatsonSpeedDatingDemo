6235c509ac30045bbf112729c91106d24cfd5ced
andywei1103 <andywei_1103@sina.com>
2018-11-14 19:40:22 -0500

    now matcher works with userEmail as parameter

--- watsonwebapp/util/match.js
+++ watsonwebapp/util/match.js
@@ -1,36 +1,37 @@
 var users = {};
 var db = require('../../util/db.js');
 
-/*
-function findMatch(personalityArr){
+function findMatch(userEmail){
     var users = db.getAllUsers();
+    var personalityArr = db.getUser(userEmail).personality;
     var personalityArr1;
     for (var user in users) {
-        personalityArr1 = user.personality();
-        var userName = user.username();
-        var i;
-        var dist;
-        var totalDist = 0;
-        var maxDist = Number.MIN_VALUE;
-        for(i = 0; i<personalityArr.length; i++){
-            dist = Math.pow((personalityArr[i]-personalityArr1[i]),2);
-            if(dist >= maxDist){
-                maxDist = dist;
+        if (user.email.valueOf() !== userEmail.valueof()) {
+            personalityArr1 = user.personality;
+            var userName = user.username;
+            var i;
+            var dist;
+            var totalDist = 0;
+            for (i = 0; i < personalityArr.length; i++) {
+                dist = Math.pow((personalityArr[i] - personalityArr1[i]), 2);
+                totalDist += dist;
             }
-            totalDist += dist;
-            totalDist = Math.sqrt(totalDist);
+            var userMap = new Map();
+            userMap.set(Math.sqrt(totalDist), userName);
+            var scores = [];
+            scores.push(totalDist);
         }
-        var userMap = new Map();
-        userMap.set(totalDist, userName);
-        var scores = [];
-        scores.push(totalDist);
-    }
-    scores.sort(function(a, b){return a - b});
-    var topMatch = [];
-    for(i=0;i<scores.length;i++){
-        topMatch.push(userMap.get(scores[i]));
+        scores.sort(function (a, b) {
+            return a - b
+        });
+        var topMatch = [];
+        for (i = 0; i < scores.length; i++) {
+            topMatch.push(userMap.get(scores[i]));
+        }
     }
     return topMatch;
 }
-*/
-module.exports = {};
+
+module.exports = {
+    findMatch : findMatch
+};
